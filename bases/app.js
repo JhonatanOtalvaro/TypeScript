d7b8c5b7191883ae93720a40b378147559fddd79
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2021-10-12 22:26:16 -0500

    Hola mundo

---
+++ bases/app.js
@@ -0,0 +1,13 @@
+//definir una constante sin el tipado estricto
+var msg = 'Hola mundo';
+console.log(msg);
+//definir constante con el tipado estricto
+var mensaje = 'Hola mundo';
+console.log(mensaje);
+//en la consola...
+/*
+==================================
+tsc = TypeScript compiler
+tsc app.ts = compilar el archivo
+
+*/ 
---
+++ bases/app.ts
@@ -0,0 +1,13 @@
+//definir una constante sin el tipado estricto
+const msg = 'Hola mundo';
+console.log(msg);
+//definir constante con el tipado estricto
+const mensaje: string = 'Hola mundo';
+console.log(mensaje);
+//en la consola...
+/*
+==================================
+tsc = TypeScript compiler
+tsc app.ts = compilar el archivo
+
+*/
