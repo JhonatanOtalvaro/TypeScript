78d29a53de919e5f848d2a2cce4abc9fed6d71dc
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2021-10-12 22:38:29 -0500

    TSconfig.json

--- bases/app.js
+++ bases/app.js
@@ -1,13 +1,15 @@
 //definir una constante sin el tipado estricto
 var msg = 'Hola mundo';
-console.log(msg);
+console.log("definir una constante sin el tipado estricto:", msg);
 //definir constante con el tipado estricto
 var mensaje = 'Hola mundo';
-console.log(mensaje);
+console.log("definir constante con el tipado estricto:", mensaje);
 //en la consola...
 /*
 ==================================
 tsc = TypeScript compiler
 tsc app.ts = compilar el archivo
+tsc --init
+tsc = generar un archivo eliminado
 
 */ 
--- bases/app.ts
+++ bases/app.ts
@@ -1,13 +1,15 @@
 //definir una constante sin el tipado estricto
 const msg = 'Hola mundo';
-console.log(msg);
+console.log("definir una constante sin el tipado estricto:", msg);
 //definir constante con el tipado estricto
 const mensaje: string = 'Hola mundo';
-console.log(mensaje);
+console.log("definir constante con el tipado estricto:", mensaje);
 //en la consola...
 /*
 ==================================
 tsc = TypeScript compiler
 tsc app.ts = compilar el archivo
+tsc --init = traducir de TypeScrip a JavaScript 
+tsc = generar un archivo eliminado
 
 */
