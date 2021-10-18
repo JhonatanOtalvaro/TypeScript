3060cc611f9e9d98aa0a6f0fc684d05a976c0877
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2021-10-18 08:58:25 -0500

    number

---
+++ bases/2-Tipos-Basicos/2.3-numbers/number.js
@@ -0,0 +1,12 @@
+"use strict";
+(function () {
+    var avengers = 10;
+    console.log(avengers);
+    var villians = 20;
+    if (avengers < villians) {
+        console.log("'Estamos en problemas'");
+    }
+    else {
+        console.log("'Nos salvamos'");
+    }
+});
