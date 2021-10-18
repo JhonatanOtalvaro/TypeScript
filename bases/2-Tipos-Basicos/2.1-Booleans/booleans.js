77375bd32ae846e53acd60448d17e4558c3aff8e
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2021-10-18 08:46:27 -0500

    Booleans

--- bases/2-Tipos-Basicos/2.1-Booleans/booleans.js
+++ bases/2-Tipos-Basicos/2.1-Booleans/booleans.js
@@ -1,1 +1,7 @@
 "use strict";
+(function () {
+    var isSuperman = true;
+    var isBatman = false;
+    console.log("SuperMan es: " + isSuperman);
+    console.log("BatMan es: " + isBatman);
+})();
---
+++ bases/2-Tipos-Basicos/2.1-Booleans/booleans.ts
@@ -0,0 +1,9 @@
+(()=>{
+
+    let isSuperman = true;
+    let isBatman = false;
+
+    console.log("SuperMan es: "+isSuperman);
+    console.log("BatMan es: "+isBatman);
+
+})()
