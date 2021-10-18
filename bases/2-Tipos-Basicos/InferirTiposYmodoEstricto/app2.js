e9781f486b727f6a02bfd519803ed691c44ceaef
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2021-10-18 08:24:28 -0500

    Renombre de archivos.

--- bases/2-Tipos-Basicos/InferirTiposYmodoEstricto/app2.js
+++ bases/2-Tipos-Basicos/InferirTiposYmodoEstricto/app2.js
@@ -1,28 +1,20 @@
-/*Función anónima autoinvocada para que las variables no choquen con 
+"use strict";
+/*Función anónima autoinvocada para que las variables no choquen con
 otras variables de otros archivos*/
-(() => {
-
+(function () {
     var a = 10;
     console.log(a);
-
-})()
-
+})();
 /* Inferir tipos y modo estricto */
 //Cualquier tipado
 //--------------------------------
 //const a = 10;
-
 //Tipo number con 'let'
 //--------------------------------
 //let b = 10;
 //--------------------------------
 /* Infiriendo los tipos */
-
 //a: es un número
-const a:number = 10;
+//const a:number = 10;
 //b: es un número
-let b:number = 10;
-
-
-
-
+//let b:number = 10;
--- bases/2-Tipos-Basicos/InferirTiposYmodoEstricto/app2.ts
+++ bases/2-Tipos-Basicos/InferirTiposYmodoEstricto/app2.ts
@@ -1,20 +1,28 @@
-"use strict";
-/*Función anónima autoinvocada para que las variables no choquen con
+/*Función anónima autoinvocada para que las variables no choquen con 
 otras variables de otros archivos*/
-(function () {
+(() => {
+
     var a = 10;
     console.log(a);
-})();
+
+})()
+
 /* Inferir tipos y modo estricto */
 //Cualquier tipado
 //--------------------------------
 //const a = 10;
+
 //Tipo number con 'let'
 //--------------------------------
 //let b = 10;
 //--------------------------------
 /* Infiriendo los tipos */
+
 //a: es un número
-var a = 10;
+//const a:number = 10;
 //b: es un número
-var b = 10;
+//let b:number = 10;
+
+
+
+
