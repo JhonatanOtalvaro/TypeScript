64c649ba2838c2b7683bfeb508dce865cdddf98d
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2021-10-25 01:40:34 -0500

    string

---
+++ bases/2-Tipos-Basicos/2.4-strings/strings.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+    var batman = 'Batman';
+    var linternaVerde = "linterna Verde";
+    var volcanNegro = "H\u00E9roe: volcan Negro";
+    console.log("I'm  " + batman);
+    console.log(batman.toUpperCase());
+})();
---
+++ bases/2-Tipos-Basicos/2.4-strings/strings.ts
@@ -0,0 +1,10 @@
+(() => {
+
+const batman: string = 'Batman';
+const linternaVerde: string = "linterna Verde";
+const volcanNegro: string = `Héroe: volcan Negro`;
+
+console.log(`I'm  ${batman}`);
+console.log(batman.toUpperCase());
+
+})()
