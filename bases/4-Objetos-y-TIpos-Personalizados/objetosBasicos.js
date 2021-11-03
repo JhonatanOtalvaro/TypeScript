34cd32787cdce16184003342ebe4945a4764bdcd
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2021-11-03 15:43:35 -0500

    Objetos básicos

---
+++ bases/4-Objetos-y-TIpos-Personalizados/objetosBasicos.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Objetos Básicos");
+console.log("---------------");
+(function () {
+    // Objeto literal
+    var flash = {
+        nombre: "Barry Allen",
+        edad: 24,
+        poderes: ["Puede correr muy rapido", "Viaja por el tiempo"]
+    };
+})();
---
+++ bases/4-Objetos-y-TIpos-Personalizados/objetosBasicos.ts
@@ -0,0 +1,13 @@
+console.log("Objetos Básicos");
+console.log("---------------");
+
+(() => {
+    // Objeto literal
+    let flash = {
+
+        nombre: "Barry Allen",
+        edad: 24,
+        poderes: ["Puede correr muy rapido", "Viaja por el tiempo"]
+
+    }
+})()
